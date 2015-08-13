947c67d5291a4e063bf028dddfb982db94fce95d
Bodin <thomas.bodin.pro@gmail.com>
2015-08-13 15:46:56 +0200

    feat (gulp) : GULP + NODE MODULE

---
+++ gulpfile.js
@@ -0,0 +1,106 @@
+var gulp = require('gulp'),
+    less = require('gulp-less'),
+    path = require('path'),
+    csso = require('gulp-csso'),
+    autoprefixer = require('gulp-autoprefixer'),
+    concat = require('gulp-concat'),
+    sourcemaps = require('gulp-sourcemaps'),
+    jshint = require('gulp-jshint'),
+    stylish = require('jshint-stylish'),
+    uglify = require('gulp-uglify'),
+    imagemin = require('gulp-imagemin'),
+    pngquant = require('imagemin-pngquant'),
+    notify = require('gulp-notify'),
+    plumber = require('gulp-plumber');
+
+
+var assets = 'wp-content/themes/__projectName__/assets/';
+
+var plumberErrorHandler = { errorHandler: notify.onError({
+    title: 'Gulp',
+    message: 'Error: <%= error.message %>'
+})};
+
+
+gulp.task('css', function () {
+    return gulp.src(assets + 'css/src/main.less')
+        .pipe(plumber(plumberErrorHandler))
+        .pipe(less({
+            paths: [ path.join(__dirname, 'less', 'includes') ]
+        }))
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
+        .pipe(csso())
+        .pipe(gulp.dest(assets + 'css/build/'));
+});
+
+
+gulp.task('js', function () {
+    return gulp.src(assets + 'js/src/**/*.js')
+        .pipe(plumber(plumberErrorHandler))
+        .pipe(jshint('.jshintrc', {fail: true}))
+        .pipe(jshint.reporter(stylish))
+        .pipe(sourcemaps.init())
+        .pipe(concat('main.js'))
+        .pipe(uglify())
+        .pipe(sourcemaps.write('maps'))
+        .pipe(gulp.dest(assets + 'js/build/'));
+});
+
+
+gulp.task('img', function () {
+    return gulp.src(assets + 'img/src/*')
+        .pipe(plumber(plumberErrorHandler))
+        .pipe(imagemin({
+            progressive: true,
+            svgoPlugins: [{removeViewBox: false}],
+            use: [pngquant()]
+        }))
+        .pipe(gulp.dest(assets + 'img/'));
+});
+
+
+gulp.task('copy', function () {
+    gulp.src('node_modules/bootstrap/dist/css/bootstrap.css')
+        .pipe(gulp.dest(assets + 'css/src/vendors/'));
+
+    gulp.src('node_modules/font-awesome/fonts/*')
+        .pipe(gulp.dest(assets + 'fonts/font-awesome/'));
+
+    gulp.src('node_modules/jquery/dist/jquery.min.js')
+        .pipe(gulp.dest(assets + 'js/src/vendors/'));
+
+    gulp.src('node_modules/html5shiv/dist/html5shiv.min.js')
+        .pipe(gulp.dest(assets + 'js/build/'));
+
+    gulp.src('node_modules/respond.js/dest/respond.min.js')
+        .pipe(gulp.dest(assets + 'js/build/'));
+});
+
+
+gulp.task('default', ['css', 'js', 'img', 'copy']);
+
+
+gulp.task('watch', function () {
+    gulp.watch(
+        assets + 'css/src/**/*.less', ['css']
+    ).on('change', function(event){
+        console.log('Le fichier ' + event.path + ' a ete modifie.');
+    });
+
+    gulp.watch(
+        assets + 'js/src/**/*.js', ['js']
+    ).on('change', function(event){
+        console.log('Le fichier ' + event.path + ' a ete modifie.');
+    }).on('error', notify.onError(function (error) {
+        return error.message;
+    }));
+
+    gulp.watch(
+        assets + 'img/src/*.{png,jpg,gif}', ['img']
+    ).on('change', function(event){
+        console.log('L\'image ' + event.path + ' a ete ajoute/modifie.');
+    });
+});
--- wp-content/themes/__projectName__/header.php
+++ wp-content/themes/__projectName__/header.php
@@ -9,25 +9,6 @@
     <link rel="profile" href="http://gmpg.org/xfn/11">
     <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 
-    <!-- Styles -->
-    <link href="<?php bloginfo( 'template_directory' ); ?>/library/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
-    <link href="<?php bloginfo( 'template_directory' ); ?>/library/css/style.css" rel="stylesheet">
-    <link rel="icon" type="image/png" href="<?php bloginfo( 'template_directory' ); ?>/img/avatar.png" />
-
-    <!-- Scripts -->
-    <script src="//code.jquery.com/jquery-1.11.2.min.js"></script>
-    <script type='text/javascript' src="<?php bloginfo( 'template_directory' ); ?>/library/js/bootstrap.min.js"></script>
-	<link href="<?php bloginfo( 'template_directory' ); ?>/library/outdatedbrowser/outdatedbrowser.min.css" rel="stylesheet">
-    <link href='http://fonts.googleapis.com/css?family=Raleway:400,100,200,300,500,600,700,800,900' rel='stylesheet' type='text/css'>
-
-    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
-
     <!-- Favicon -->
     <link rel="apple-touch-icon" sizes="57x57" href="<?php bloginfo( 'template_directory' ); ?>/assets/favicon/apple-touch-icon-57x57.png">
     <link rel="apple-touch-icon" sizes="60x60" href="<?php bloginfo( 'template_directory' ); ?>/assets/favicon/apple-touch-icon-60x60.png">
@@ -46,6 +27,26 @@
     <meta name="msapplication-TileColor" content="#000000">
     <meta name="msapplication-TileImage" content="<?php bloginfo( 'template_directory' ); ?>/assets/favicon/mstile-144x144.png">
     <meta name="theme-color" content="#ffffff">
+
+
+    <!-- Styles -->
+    <link href="<?php bloginfo( 'template_directory' ); ?>/assets/css/build/main.css" rel="stylesheet">
+    <link href="<?php bloginfo( 'template_directory' ); ?>/assets/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+    <link href="<?php bloginfo( 'template_directory' ); ?>/library/css/style.css" rel="stylesheet">
+    <link rel="icon" type="image/png" href="<?php bloginfo( 'template_directory' ); ?>/img/avatar.png" />
+
+    <!-- Scripts -->
+    <script src="//code.jquery.com/jquery-1.11.2.min.js"></script>
+    <script type='text/javascript' src="<?php bloginfo( 'template_directory' ); ?>/library/js/bootstrap.min.js"></script>
+    <link href="<?php bloginfo( 'template_directory' ); ?>/library/outdatedbrowser/outdatedbrowser.min.css" rel="stylesheet">
+    <link href='http://fonts.googleapis.com/css?family=Raleway:400,100,200,300,500,600,700,800,900' rel='stylesheet' type='text/css'>
+
+
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
 
     <?php wp_head(); ?>
 </head>
