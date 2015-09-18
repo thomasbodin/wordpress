db0a24bc8c7fc7ebd6e96876e810aafe3828cafb
Bodin <thomas.bodin.pro@gmail.com>
2015-09-18 13:17:01 +0200

    fix (title): Title page dynamique
    issue #36

--- wp-content/themes/__projectName__/header.php
+++ wp-content/themes/__projectName__/header.php
@@ -3,7 +3,7 @@
     <!--[if IE 8]><html class="ie ie8" <?php language_attributes(); ?>><![endif]-->
     <!--[if !(IE 7) | !(IE 8)  ]><!--><html <?php language_attributes(); ?>><!--<![endif]-->
     <head>
-        <title>Nouveau projet</title>
+        <title><?php the_title(); ?></title>
         <meta charset="<?php bloginfo( 'charset' ); ?>">
         <meta name="viewport" content="width=device-width">
         <link rel="profile" href="http://gmpg.org/xfn/11">
