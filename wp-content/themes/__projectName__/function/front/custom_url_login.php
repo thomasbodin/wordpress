aa790e24e6bc1aa2d605e3d9945c0f37720131f6
Salahkar <vincent.salahkar@edu.esiee.fr>
2015-10-06 11:54:16 +0200

    fix (bloginfo) : site url deprecie
    chgmnt argument

--- wp-content/themes/__projectName__/function/front/custom_url_login.php
+++ wp-content/themes/__projectName__/function/front/custom_url_login.php
@@ -4,6 +4,6 @@
      * siteurl du login
      */
     function custom_url_login() {
-        return get_bloginfo( 'siteurl' );
+        return get_bloginfo( 'url' );
     }
     add_filter('login_headerurl', 'custom_url_login');
