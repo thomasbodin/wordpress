07e99d92f86f73a237c1cdc7fc7b0f8b22dec28b
Bodin <thomas.bodin.pro@gmail.com>
2016-04-25 15:48:30 +0200

    fix (function): Actif par defaut
    CPT, Menu & page option acf

--- wp-content/themes/__projectName__/function/admin.php
+++ wp-content/themes/__projectName__/function/admin.php
@@ -12,7 +12,7 @@
 
     require_once('admin/add_theme_support.php');
     require_once('admin/yoast.php');
-    //require_once('admin/page_option_acf.php');
+    require_once('admin/page_option_acf.php');
     //require_once('admin/wpadmin_filter.php');
 
 
--- wp-content/themes/__projectName__/function/all.php
+++ wp-content/themes/__projectName__/function/all.php
@@ -10,9 +10,9 @@
      *
      */
 
-    //require_once('all/cpt.php');
+    require_once('all/cpt.php');
     //require_once('all/log_user.php');
-    //require_once('admin/menu.php');
+    require_once('all/menu.php');
     //require_once('admin/wp_bootstrap_navwalker.php');
 
 
