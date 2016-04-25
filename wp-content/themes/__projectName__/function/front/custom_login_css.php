2e627b7685cabbd6f399991c6701e86db0d11a2b
Bodin <thomas.bodin.pro@gmail.com>
2016-04-25 15:58:22 +0200

    fix (login): Style bouton
    Suppression de l'ombre sur le texte du bouton

--- wp-content/themes/__projectName__/function/front/custom_login_css.php
+++ wp-content/themes/__projectName__/function/front/custom_login_css.php
@@ -4,6 +4,6 @@
      * Login CSS custom
      */
     function custom_login_css(){
-        echo '<link rel="stylesheet" type="text/css" href="' . get_bloginfo('template_directory') . '/css/build/lecentre.css" />';
+        echo '<link rel="stylesheet" type="text/css" href="' . get_bloginfo('template_directory') . '/style/build/lecentre.css" />';
     }
     add_action('login_head', 'custom_login_css');
