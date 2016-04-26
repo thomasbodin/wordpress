26d30bfb8d03354730c5ca26b1806657adf36ac3
Bodin <thomas.bodin.pro@gmail.com>
2016-04-26 17:06:18 +0200

    fix (config): Correctif dû à la MAJ WP

--- wp-config-sample.php
+++ wp-config-sample.php
@@ -2,17 +2,18 @@
 /**
  * La configuration de base de votre installation WordPress.
  *
- * Ce fichier contient les réglages de configuration suivants : réglages MySQL,
- * préfixe de table, clefs secrètes, langue utilisée, et ABSPATH.
- * Vous pouvez en savoir plus à leur sujet en allant sur
- * {@link http://codex.wordpress.org/fr:Modifier_wp-config.php Modifier
- * wp-config.php}. C'est votre hébergeur qui doit vous donner vos
- * codes MySQL.
+ * Le script de création wp-config.php utilise ce fichier lors de l'installation.
+ * Vous n'avez pas à utiliser l'interface web, vous pouvez directement
+ * renommer ce fichier en "wp-config.php" et remplir les variables à la main.
  *
- * Ce fichier est utilisé par le script de création de wp-config.php pendant
- * le processus d'installation. Vous n'avez pas à utiliser le site web, vous
- * pouvez simplement renommer ce fichier en "wp-config.php" et remplir les
- * valeurs.
+ * Ce fichier contient les configurations suivantes :
+ *
+ * * réglages MySQL ;
+ * * clefs secrètes ;
+ * * préfixe de tables de la base de données ;
+ * * ABSPATH.
+ *
+ * @link https://codex.wordpress.org/Editing_wp-config.php
  *
  * @package WordPress
  */
@@ -34,8 +35,8 @@ define('DB_HOST', 'localhost');
 define('DB_CHARSET', 'utf8');
 
 /** Type de collation de la base de données.
-  * N'y touchez que si vous savez ce que vous faites.
-  */
+ * N'y touchez que si vous savez ce que vous faites.
+ */
 define('DB_COLLATE', '');
 
 /**#@+
@@ -49,14 +50,14 @@ define('DB_COLLATE', '');
  *
  * @since 2.6.0
  */
-define('AUTH_KEY',         'put your unique phrase here');
-define('SECURE_AUTH_KEY',  'put your unique phrase here');
-define('LOGGED_IN_KEY',    'put your unique phrase here');
-define('NONCE_KEY',        'put your unique phrase here');
-define('AUTH_SALT',        'put your unique phrase here');
-define('SECURE_AUTH_SALT', 'put your unique phrase here');
-define('LOGGED_IN_SALT',   'put your unique phrase here');
-define('NONCE_SALT',       'put your unique phrase here');
+define('AUTH_KEY',         '>7F{]oHmdh?:uV_AY~ia(&,uz`AIfh]3!9Tz/4. *vKB@Wfbm&eKs}}9MnFmv} ~');
+define('SECURE_AUTH_KEY',  '3h/zje+@>98VQd5g#02>}nY?+.+BrzrhB~e#%$u2W/beG-w4J@Z.J9`RIT-%~VaG');
+define('LOGGED_IN_KEY',    'FI-UR{41)UmYQFt92]@t6swIB<;g}/-rO)|y}~kPA!w.ILp?:Dx|6){Ks|vxxXuk');
+define('NONCE_KEY',        'b2e|rK^S(At@!_H6%q,sI%`jwAhn&PJ([uA^Q~/2C=Cb!7G>hsJbWfl>;zTn=d|&');
+define('AUTH_SALT',        ',GqD]hx-8)yl)U9PrlE.tIq)3eu=ig8q!#)D1CG|jg1k4Q1h@MN;~*[5D(L>-#&x');
+define('SECURE_AUTH_SALT', '*CDDi%GN>fM FmV#N D&@wTLU-MAB_?JC?{Zy&n<~sYn/jPTO]n}t>-^[++|d(_m');
+define('LOGGED_IN_SALT',   't)ts9cuW/JfX |do<mvmTI9cMZYpAtVZ[/d-9aJ]EE-YBUU^-Wi}4miA)6RJ]8`P');
+define('NONCE_SALT',       '|@zn ]!p:sD  &)@u:+/9LhD5rKw*8T0H^c>){r!C=L8;n429T<cB]`gUsYsW9_ ');
 /**#@-*/
 
 /**
@@ -66,23 +67,38 @@ define('NONCE_SALT',       'put your unique phrase here');
  * si vous leur donnez chacune un préfixe unique.
  * N'utilisez que des chiffres, des lettres non-accentuées, et des caractères soulignés!
  */
-$table_prefix  = 'wp_';
+$table_prefix  = 'bltzr_';
+
+/**
+ * Activation du debug WP si le site est :
+ * en local
+ * sur baltazarestudio.fr
+ */
+function in_dev(){
+	$devServ = [
+			'localhost',
+			'bltzr.fr',
+			'127.0.0.1',
+	];
+
+	return (in_array( $_SERVER['SERVER_NAME'],$devServ));
+}
 
 /**
  * Pour les développeurs : le mode déboguage de WordPress.
  *
  * En passant la valeur suivante à "true", vous activez l'affichage des
- * notifications d'erreurs pendant vos essais.
+ * notifications d'erreurs pendant votre essais.
  * Il est fortemment recommandé que les développeurs d'extensions et
  * de thèmes se servent de WP_DEBUG dans leur environnement de
  * développement.
  *
- * Pour plus d'information sur les autres constantes qui peuvent être utilisées
- * pour le déboguage, rendez-vous sur le Codex.
- * 
- * @link https://codex.wordpress.org/Debugging_in_WordPress 
+ * Pour obtenir plus d'information sur les constantes
+ * qui peuvent être utilisée pour le déboguage, consultez le Codex.
+ *
+ * @link https://codex.wordpress.org/Debugging_in_WordPress
  */
-define('WP_DEBUG', false);
+define('WP_DEBUG', in_dev());
 
 /* C'est tout, ne touchez pas à ce qui suit ! Bon blogging ! */
 
