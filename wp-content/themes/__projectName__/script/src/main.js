2f5b756ecc22c07d35f8299dcde1a55ff5abe8e6
mathieubarnetche <mathieu.barnetche@gmail.com>
2016-08-05 11:50:10 +0200

    feat-cookiecnil :
    Le cookie s'en va désormais au moindre clic sur la page ou au moindre scroll

--- wp-content/themes/__projectName__/script/src/main.js
+++ wp-content/themes/__projectName__/script/src/main.js
@@ -2,31 +2,16 @@ function cookieCnil(){
     if ($.cookie('cookieCnil') === undefined) {
         $('#cookieCnil').fadeIn();
 
-        $(function () {
-            $(window).click(function () {
+        $(window).click(function () {
+            $('#cookieCnil').fadeOut(300);
+            $.cookie('cookieCnil', "viewed", {expires: 365});
+        });
+
+        $(window).scroll(function () {
+            if ($(this).scrollTop() > 1) {
                 $('#cookieCnil').fadeOut(300);
                 $.cookie('cookieCnil', "viewed", {expires: 365});
-            });
-
-            $(window).scroll(function () {
-                if ($(this).scrollTop() > 1) {
-                    $('#cookieCnil').fadeOut(300);
-                    $.cookie('cookieCnil', "viewed", {expires: 365});
-                }
-            });
-        });
-
-        $('#cookie_btn_ok').click(function(e){
-            e.preventDefault();
-            $('#cookieCnil').fadeOut();
-            $.cookie('cookieCnil', "viewed", {expires: 365});
-        });
-
-        $('#cookie_btn_refuser').click(function(e){
-            e.preventDefault();
-            $('#cookieCnil').fadeOut();
-            $.cookie('cookieCnil', "viewed", {expires: 365});
-            $.cookie('cookieRefuser', "1", {expires: 365});
+            }
         });
     }
 }
